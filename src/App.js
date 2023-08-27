8962b5b09982cc383108926eae77486a98eee047
varun <varunreddymullangi@gmail.com>
2023-08-26 22:46:12 -0700

    added travis deploy config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hi there!
+          Edit <code>src/App.js</code> May the force be with you.
         </p>
         <a
           className="App-link"
